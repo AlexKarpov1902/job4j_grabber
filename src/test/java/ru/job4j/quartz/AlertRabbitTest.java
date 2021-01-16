d3c73186976733838e271c86738d5ab0af65789a
AlexKarpov1902 <leb34359@gmail.com>
2021-01-16 19:57:55 +0500

    Quartz [#428333]

--- src/test/java/ru/job4j/quartz/AlertRabbitTest.java
+++ src/test/java/ru/job4j/quartz/AlertRabbitTest.java
@@ -3,9 +3,9 @@ package ru.job4j.quartz;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
-public class AlertRabbitTest{
+public class AlertRabbitTest {
         @Test
         public void test() {
             Assert.assertEquals(1, 1);
