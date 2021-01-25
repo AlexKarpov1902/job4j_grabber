853aa79ab051ab9911ccf754a65ef2c7ea2f421d
AlexKarpov1902 <leb34359@gmail.com>
2021-01-25 23:49:43 +0500

    Преобразование даты [#428343]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,17 +5,50 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException, ParseException {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
+        DateFormatSymbols dfs = DateFormatSymbols.getInstance();
+        dfs.setShortMonths(new String[]{
+                "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"});
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yy, HH:mm", dfs);
+
         for (Element td : row) {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
             Elements sibling = td.siblingElements();
-            System.out.println(sibling.get(4).text());
+            String txtDate = sibling.get(4).text();
+            Date date = null;
+            try {
+                date = sdf.parse(txtDate);
+            } catch (ParseException e) {
+                if (txtDate.startsWith("сегодня")) {
+                    String newDate = sdf.format(new Date());
+                    String s = newDate.substring(0, 11) + txtDate.substring(9);
+                    date = sdf.parse(s);
+                } else if (txtDate.startsWith("вчера")) {
+                    String newDate = sdf.format(new Date(System.currentTimeMillis() - 86_400_000L));
+                    String s = newDate.substring(0, 11) + txtDate.substring(7);
+                    date = sdf.parse(s);
+                } else {
+                    System.out.println(e.getMessage() + "-" + e.getErrorOffset());
+                }
+             }
+            System.out.println("дата: " + date);
         }
     }
-
 }
+
