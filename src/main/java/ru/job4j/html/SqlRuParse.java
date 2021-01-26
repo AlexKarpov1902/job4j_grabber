8ec7be34989c7b8f7fcb1e1edf4ddd805f3d8632
AlexKarpov1902 <leb34359@gmail.com>
2021-01-26 09:52:26 +0500

    Парсинг https://www.sql.ru/forum/job-offers/3 [#428339]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,21 +9,28 @@ import java.io.IOException;
 import java.text.DateFormatSymbols;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
+
 
 public class SqlRuParse {
-    public static void main(String[] args) throws IOException, ParseException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+
+    private String[] shortMonths = new String[]{
+            "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"};
+    private SimpleDateFormat simpleDateFormat;
+    private DateFormatSymbols dateFormatSymbols;
+
+    public SqlRuParse() {
+        this.dateFormatSymbols = DateFormatSymbols.getInstance();
+        dateFormatSymbols.setShortMonths(shortMonths);
+        this.simpleDateFormat = new SimpleDateFormat("dd MMM yy, HH:mm", dateFormatSymbols);
+
+    }
+
+    public void parse(String link) throws IOException, ParseException {
+
+        Document doc = Jsoup.connect(link).get();
         Elements row = doc.select(".postslisttopic");
-        DateFormatSymbols dfs = DateFormatSymbols.getInstance();
-        dfs.setShortMonths(new String[]{
-                "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"});
-        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yy, HH:mm", dfs);
 
         for (Element td : row) {
             Element href = td.child(0);
@@ -33,22 +40,37 @@ public class SqlRuParse {
             String txtDate = sibling.get(4).text();
             Date date = null;
             try {
-                date = sdf.parse(txtDate);
+                date = simpleDateFormat.parse(txtDate);
             } catch (ParseException e) {
                 if (txtDate.startsWith("сегодня")) {
-                    String newDate = sdf.format(new Date());
+                    String newDate = simpleDateFormat.format(new Date());
                     String s = newDate.substring(0, 11) + txtDate.substring(9);
-                    date = sdf.parse(s);
+                    date = simpleDateFormat.parse(s);
                 } else if (txtDate.startsWith("вчера")) {
-                    String newDate = sdf.format(new Date(System.currentTimeMillis() - 86_400_000L));
+                    String newDate = simpleDateFormat.format(new Date(System.currentTimeMillis() - 86_400_000L));
                     String s = newDate.substring(0, 11) + txtDate.substring(7);
-                    date = sdf.parse(s);
+                    date = simpleDateFormat.parse(s);
                 } else {
                     System.out.println(e.getMessage() + "-" + e.getErrorOffset());
                 }
-             }
+            }
             System.out.println("дата: " + date);
         }
+
     }
+
+
+    public static void main(String[] args) throws IOException, ParseException {
+        SqlRuParse sqlRuParse = new SqlRuParse();
+        String link = "https://www.sql.ru/forum/job-offers/";
+
+        for (int i = 1; i < 6; i++) {
+            link = link.concat(i == 1 ? "" : String.valueOf(i));
+            System.out.printf(" ------------------------- Страница %d -------------------------------%s", i, System.lineSeparator());
+            sqlRuParse.parse(link);
+        }
+
+    }
+
 }
 
